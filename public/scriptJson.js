31ff0083ff0bfcd042f85d571d1e7bbaf9e08227
GO354146 <gobika.k86@wipro.com>
2017-06-08 19:41:02 +0530

    initial

---
+++ public/scriptJson.js
@@ -0,0 +1,38 @@
+var app = angular
+            .module("myModule",[])
+            .controller("myController",function($scope, $http){
+              $http ({
+                method: 'GET',
+                url: 'http://localhost:8081/books'
+              })
+              .then(function(res){
+                console.log(res.data);
+                console.log("jguyfg");
+                $scope.books = res.data;
+
+            });
+            var ratings = [  { star: "5 star", selected: true}, { star: "4 star", selected: false }, { star: "3 star", selected: false }, { star: "2 star", selected: false }, { star: "1 star", selected: false } ];
+              $scope.ratings = ratings;
+
+               $scope.addEntry = function(data) {
+                var tempObj = {
+                  star: data.star,
+                  email: data.email,
+                  comment: data.comment
+                }
+                data.reviews.push(tempObj);
+                delete data.star;
+                delete data.email;
+                delete data.comment;
+           //     delete data.star;
+                console.log(data);
+                $http ({
+                    method: 'PUT',
+                    url: 'http://localhost:8081/books/' + data.id,
+                    data: data
+               })
+            
+              }
+ 
+
+          });
